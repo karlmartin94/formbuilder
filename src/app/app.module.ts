cb9db046d2386803f4761cb39ebee1ccba845b93
karlmartin94 <karlmartin94@outlook.com>
2017-06-10 20:23:53 +0100

    created color picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from "./app-routing.module";
+
+
 import { AppComponent } from './app.component';
 import { DesignerComponent } from "./designer.component";
 import { HomeComponent } from "./home.component";
-import {AppRoutingModule} from "./app-routing.module";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignerComponent,
+    HomeComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
       HttpModule,
-      AppRoutingModule
+      AppRoutingModule,
+
+      NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [
-      AppComponent,
-      DesignerComponent,
-      HomeComponent
+      AppComponent
   ]
 })
 export class AppModule { }
--- src/app/designer.component.ts
+++ src/app/designer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as data from './colors.json';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
-  selector: 'designer',
-  templateUrl: './designer.component.html',
-  styleUrls: ['./designer.component.css']
+    selector: 'designer',
+    templateUrl: './designer.component.html',
+    styleUrls: ['./designer.component.css']
 })
 export class DesignerComponent {
-  title = 'Designer works!';
+    private colorSchemes;
+    private selectedColor;
+
+    constructor(private sanitizer: DomSanitizer) {
+        this.colorSchemes = (<any>data).colorSchemes;
+        this.selectedColor = this.colorSchemes[0];
+    }
+
+    setSelectedColor(newSelectedColor: any): void{
+        this.selectedColor = newSelectedColor;
+    }
+
+    setBackgroundColor(event: any, color: String){
+        event.target.style.backgroundColor = color;
+    }
 }
+
